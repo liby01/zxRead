e2f6004acfc1f35673464d7703c68509405f34ff
liby <Liboyang19965263@gmail.com>
2024-06-29 16:25:54 +0800

    重构
    重构

---
+++ zx-parent/zx-service/service-cart/src/main/java/com/by/CartApplication.java
@@ -0,0 +1,12 @@
+package com.by;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)  // 排除数据库的自动化配置，Cart微服务不需要访问数据库
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class , args) ;
+    }
+}
